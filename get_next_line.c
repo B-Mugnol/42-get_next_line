8cb62f44c2c73737bd2c6521054aa4107fa4ea2b
bmugnol- <bmugnol-@student.42sp.org.br>
2021-10-22 20:20:10 -0300

    hm

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,45 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:22:36 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/22 19:39:28 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/10/22 20:17:13 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// read() < BUFFER_SIZE === end of file;
-
 char	*get_next_line(int fd)
 {
 	static char	*backup;
+	ssize_t		read_val;
 	char		*aux;
 	char		*buffer;
 	char		*new_line;
 
-	if (!backup)
+	if (backup)
 	{
 		buffer = ft_strndup(backup, ft_strlen(backup));
 		free(backup);
+		backup = NULL;
+		new_line = ft_strchr(buffer, '\n');
+		if (new_line)
+		{
+			backup = ft_strndup(new_line + 1, ft_strlen(new_line + 1));
+			aux = ft_strndup(buffer, new_line - buffer);
+			free(buffer);
+			return (aux);
+		}
 		return (buffer);
 	}
 	buffer = malloc((BUFFER_SIZE) * sizeof(char));
-	if (fd == -1 || !buffer)
-		return (NULL);
-	if (read(fd, buffer, BUFFER_SIZE) <= 0)
+	read_val = read(fd, buffer, BUFFER_SIZE);
+	if (read_val <= 0 || fd == -1 || !buffer)
 	{
 		free(buffer);
 		free(backup);
 		return (NULL);
 	}
+	else if (read_val < BUFFER_SIZE)
+		*(buffer + read_val) = '\0';
 	new_line = ft_strchr(buffer, '\n');
 	if (new_line)
 	{
