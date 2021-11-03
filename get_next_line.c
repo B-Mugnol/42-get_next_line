29d049de9952f0ca0ae048129ccc4a610be53ba3
bmugnol- <bmugnol-@student.42sp.org.br>
2021-11-03 14:25:20 -0300

    testing null_free necessity in some cases

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:22:36 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/11/03 14:11:38 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/11/03 14:26:13 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ static char	*do_thing(char **acc)
 	char	*result;
 	char	*aux;
 
-	if (!*acc) // || !**acc
+	if (!*acc || !**acc) //
 		return (NULL);
 	// acc isn't properly null terminated?
 	acc_nl = ft_strchr(*acc, '\n');
@@ -57,11 +57,11 @@ static char	*do_thing(char **acc)
 		// write(1, "0: ", 3);
 		aux = ft_strndup(acc_nl + 1, ft_strlen(acc_nl + 1));
 		result = ft_strndup(*acc, acc_nl - *acc + 1);
-		null_free(acc);
-		// free(*acc);
+		// null_free(acc);
+		free(*acc);
 		*acc = ft_strndup(aux, ft_strlen(aux));
-		null_free(&aux);
-		// free(aux);
+		// null_free(&aux);
+		free(aux);
 		return (result);
 	}
 	return (NULL);
@@ -110,16 +110,16 @@ static char	*build_line(int fd, char **acc, char **buffer, size_t buffer_len)
 		{
 			// write(1, "1: ", 3);
 			aux = ft_strnjoin(*acc, *buffer, ft_strlen(*acc), 1);
-			null_free(acc);
-			// free(*acc);
+			// null_free(acc);
+			free(*acc);
 			*acc = ft_strndup(nl + 1, ft_strlen(nl + 1));
 		}
 		else if (*(nl + 1) != '\0')
 		{
 			// write(1, "2: ", 3);
 			aux = ft_strnjoin(*acc, *buffer, ft_strlen(*acc), nl - *buffer + 1);
-			null_free(acc);
-			// free(*acc);
+			// null_free(acc);
+			free(*acc);
 			*acc = ft_strndup(nl + 1, ft_strlen(nl + 1));
 		}
 		else if (*(nl + 1) == '\0')
@@ -136,8 +136,8 @@ static char	*build_line(int fd, char **acc, char **buffer, size_t buffer_len)
 			null_free(acc);
 			// free(*acc);
 		}
-		null_free(buffer);
-		// free(*buffer);
+		// null_free(buffer);
+		free(*buffer);
 		// write(1, "out", 3);
 		return (aux);
 	}
@@ -146,21 +146,21 @@ static char	*build_line(int fd, char **acc, char **buffer, size_t buffer_len)
 		// write(1, "5: ", 3);
 		aux = ft_strnjoin(*acc, *buffer, ft_strlen(*acc), buffer_len);
 		// printf("\\%lu/", ft_strlen(aux));
-		null_free(acc);
-		// free(*acc);
-		null_free(buffer);
-		// free(*buffer);
+		// null_free(acc);
+		free(*acc);
+		// null_free(buffer);
+		free(*buffer);
 		*buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 		if (!*buffer)
 			return (NULL);
 		*acc = ft_strndup(aux, ft_strlen(aux));
-		null_free(&aux);
-		// free(aux);
+		// null_free(&aux);
+		free(aux);
 		read_val = read(fd, *buffer, BUFFER_SIZE);
 		if (read_val < 0 || fd < 0)
 		{
-			null_free(buffer);
-			// free(*buffer);
+			// null_free(buffer);
+			free(*buffer);
 			null_free(acc);
 			// free(*acc);
 			return (NULL);
@@ -168,14 +168,14 @@ static char	*build_line(int fd, char **acc, char **buffer, size_t buffer_len)
 		*(*buffer + read_val) = '\0';
 		if (read_val == 0 && *acc) //  && **acc
 		{
-			null_free(buffer);
-			// free(*buffer);
+			// null_free(buffer);
+			free(*buffer);
 			return (*acc);
 		}
 		if (read_val == 0)
 		{
-			null_free(buffer);
-			// free(*buffer);
+			// null_free(buffer);
+			free(*buffer);
 			null_free(acc);
 			// free(*acc);
 			return (NULL);
@@ -184,8 +184,8 @@ static char	*build_line(int fd, char **acc, char **buffer, size_t buffer_len)
 		{
 			// *(*buffer + read_val) = '\0';
 			aux = ft_strnjoin(*acc, *buffer, ft_strlen(*acc), read_val);
-			null_free(buffer);
-			// free(*buffer);
+			// null_free(buffer);
+			free(*buffer);
 			null_free(acc);
 			// free(*acc);
 			return (aux);
